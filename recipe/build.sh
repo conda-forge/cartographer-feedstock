c32981a4ccac8186fdf6116a0162f293243180f6
Tobias Fischer <info@tobiasfischer.info>
2023-02-17 07:32:10 +1000

    Try again

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,14 +7,17 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc"
 fi
 
+# See https://github.com/abseil/abseil-cpp/releases/tag/20230125.0
+# We need to wait for upstream to use the new flags
+export CXXFLAGS="$CXXFLAGS -DABSL_LEGACY_THREAD_ANNOTATIONS=1"
+
 cmake ${CMAKE_ARGS} .. \
       -G "Ninja" \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_CXX_STANDARD=17 \
       -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=ON \
       -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DBUILD_SHARED_LIBS=ON \
-      -DABSL_LEGACY_THREAD_ANNOTATIONS=1
+      -DBUILD_SHARED_LIBS=ON
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
