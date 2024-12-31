719b244eaf280fffcbb3ee129882399897252d0b
Tobias Fischer <info@tobiasfischer.info>
2024-12-31 13:15:05 +1000

    export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,8 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc"
 fi
 
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
+
 # See https://github.com/abseil/abseil-cpp/releases/tag/20230125.0
 # We need to wait for upstream to use the new flags
 export CXXFLAGS="$CXXFLAGS -DABSL_LEGACY_THREAD_ANNOTATIONS=1"
